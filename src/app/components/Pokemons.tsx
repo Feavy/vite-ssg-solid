b30630d946e82d3caaa9f415e71f1a11a510e764
Feavy <quentin071100@live.fr>
2023-06-13 17:49:22 +0200

    SSG data fetch POC.

--- src/app/components/Pokemons.tsx
+++ src/app/components/Pokemons.tsx
@@ -1,5 +1,5 @@
 import type {Component, Resource, Setter} from "solid-js";
-import {createResource, Show} from "solid-js";
+import {createResource, Show, Suspense} from "solid-js";
 
 interface PokemonData {
 	count: number;
@@ -17,18 +17,23 @@ interface PageProps {
 let serverCallCount = 0;
 let succeededServerCallCount = 0;
 let pageDataCache: any[] = [];
+let expectedSuccessCount = 0;
 
 function server<T>(callback: () => Promise<T>): [Resource<T>, Setter<T | undefined>] {
 	let callIndex = serverCallCount++;
+	console.log("server")
 	const [res, {mutate}] = createResource((async () => {
 		let data: T = null;
 		if (import.meta.env.SSR) {
 			// Either call callback to fetch data (generation time)
 			data = await callback();
+			if(expectedSuccessCount === 0) {
+				expectedSuccessCount = serverCallCount;
+			}
 			succeededServerCallCount++;
 			pageDataCache[callIndex] = data;
 
-			if(serverCallCount === succeededServerCallCount) {
+			if(expectedSuccessCount === succeededServerCallCount) {
 				const fs = await import("fs/promises");
 				try {
 					console.log("Create file")
@@ -67,7 +72,13 @@ function shuffle<T>(array: T[]): T[] {
 const Page: Component<PageProps> = (props) => {
 	const [pokemons, setPokemons] = server<PokemonData["results"]>(async () => {
 		return (await (fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')).then((res) => res.json())).results;
-	})
+	});
+
+	const [joke] = server<string>(async () => {
+		let joke = (await (fetch('https://v2.jokeapi.dev/joke/Programming')).then((res) => res.json()));
+		joke = joke.joke || joke.setup + "\n" + joke.delivery;
+		return joke;
+	});
 
 	function shuffleList(e) {
 		setPokemons(shuffle(pokemons()!!));
@@ -76,17 +87,20 @@ const Page: Component<PageProps> = (props) => {
 	return (
 		<div>
 			<h1>PAGE</h1>
-			<Show when={pokemons()}>
-				<h1>{pokemons()!!.length} Pokémons</h1>
+			<Suspense fallback={<h1>Loading...</h1>}>
+				<h1>{joke()}</h1>
+			</Suspense>
+			<Suspense fallback={<h1>Loading...</h1>}>
+				<h1>{pokemons()?.length} Pokémons</h1>
 				<button onclick={shuffleList}>shuffle</button>
 				<ul>
-					{pokemons()!!.map((pokemon) => (
+					{pokemons()?.map((pokemon) => (
 						<li>
 							{pokemon.name}
 						</li>
 					))}
 				</ul>
-			</Show>
+			</Suspense>
 		</div>
 	)
 };

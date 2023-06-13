eeb1c2d9f2ac501462591307850cec68ca61518c
Feavy <quentin071100@live.fr>
2023-06-13 10:50:49 +0200

    SSG data fetch POC.

--- src/app/components/Pokemons.tsx
+++ src/app/components/Pokemons.tsx
@@ -1,5 +1,5 @@
-import type {Component, Resource} from "solid-js";
-import {createResource} from "solid-js";
+import type {Component, Resource, Setter} from "solid-js";
+import {createResource, Show} from "solid-js";
 
 interface PokemonData {
 	count: number;
@@ -17,11 +17,10 @@ interface PageProps {
 let serverCallCount = 0;
 let succeededServerCallCount = 0;
 let pageDataCache: any[] = [];
-let mutates = [];
 
-function server<T>(callback: () => Promise<T>): Resource<T> {
+function server<T>(callback: () => Promise<T>): [Resource<T>, Setter<T | undefined>] {
 	let callIndex = serverCallCount++;
-	const [res] = createResource((async () => {
+	const [res, {mutate}] = createResource((async () => {
 		let data: T = null;
 		if (import.meta.env.SSR) {
 			// Either call callback to fetch data (generation time)
@@ -42,7 +41,7 @@ function server<T>(callback: () => Promise<T>): Resource<T> {
 			}
 		} else {
 			// Or fetch data generated earlier (runtime)
-			if(pageDataCache.length == 0) {
+			if (pageDataCache.length == 0) {
 				pageDataCache = await fetch("/page2/page_data.json").then((res) => res.json());
 			}
 			callIndex %= pageDataCache.length;
@@ -50,38 +49,44 @@ function server<T>(callback: () => Promise<T>): Resource<T> {
 		}
 		return data;
 	}));
-	return res;
+	return [res, mutate];
+}
+
+function shuffle<T>(array: T[]): T[] {
+	array = [...array];
+	let currentIndex = array.length, randomIndex;
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		//@ts-ignore
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
+	return array;
 }
 
 const Page: Component<PageProps> = (props) => {
-	const pokemons = server(async () => {
-		return (await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')).json();
+	const [pokemons, setPokemons] = server<PokemonData["results"]>(async () => {
+		return (await (fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')).then((res) => res.json())).results;
 	})
 
-	const joke = server(async () => {
-		return (await fetch('https://v2.jokeapi.dev/joke/Programming')).json();
-	});
+	function shuffleList(e) {
+		setPokemons(shuffle(pokemons()!!));
+	}
 
 	return (
 		<div>
-			<div>
-				<pre>{JSON.stringify(pokemons(), null, 2)}</pre>
-			</div>
-			<div>
-				<pre>{JSON.stringify(joke(), null, 2)}</pre>
-			</div>
-			{/*{data.results && (*/}
-			{/*	<>*/}
-			{/*		<h1>{data.count} Pokémons</h1>*/}
-			{/*		<ul>*/}
-			{/*			{data.results.map((pokemon) => (*/}
-			{/*				<li>*/}
-			{/*					{pokemon.name}*/}
-			{/*				</li>*/}
-			{/*			))}*/}
-			{/*		</ul>*/}
-			{/*	</>*/}
-			{/*)}*/}
+			<h1>PAGE</h1>
+			<Show when={pokemons()}>
+				<h1>{pokemons()!!.length} Pokémons</h1>
+				<button onclick={shuffleList}>shuffle</button>
+				<ul>
+					{pokemons()!!.map((pokemon) => (
+						<li>
+							{pokemon.name}
+						</li>
+					))}
+				</ul>
+			</Show>
 		</div>
 	)
 };
